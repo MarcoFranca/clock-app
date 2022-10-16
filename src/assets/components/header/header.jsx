c82dfd4a164dbc60366ea618adb1b418c346082d
Marco Franca <dev.marcotullio@gmail.com>
2022-10-16 11:55:22 -0300

    function for sending quotes [UPDATE]

--- src/assets/components/header/header.jsx
+++ src/assets/components/header/header.jsx
@@ -1,15 +1,30 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {HeaderContainer} from "./headerStyle";
-
+import {getQuotes} from "../../services/api/apiRote";
+import refreshImg from "../../images/desktop/icon-refresh.svg"
 
 export function Header() {
+    const [cote, setCote] = useState()
+
+    useEffect(()=>{
+        getQuotes(setCote)
+    },[])
+
 
     return(
-        <HeaderContainer>
-            <div className="quotes">
+        !cote? 'loading...':
+            <HeaderContainer>
+                <div className="quotes">
+                    <h1>
+                        “{cote.en}”
+                    </h1>
+                    <p>{cote.author}</p>
 
-                <img src="" alt=""/>
-            </div>
-        </HeaderContainer>
+                </div>
+                <div className="image" onClick={()=>getQuotes(setCote)}>
+                    <img src={refreshImg} alt="refresh icon"/>
+                </div>
+            </HeaderContainer>
+
     )
 }
