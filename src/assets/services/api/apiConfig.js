14de978d254f9c79e3d431d0df0f7e20e00329dd
Marco Franca <dev.marcotullio@gmail.com>
2022-10-18 13:53:09 -0300

    update config [UPDATE]

--- src/assets/services/api/apiConfig.js
+++ src/assets/services/api/apiConfig.js
@@ -1,13 +1,13 @@
 export const api = {
     worldTime: {
         curl:"http://worldtimeapi.org/api/timezone",
-        area:"",
-        location:"",
-        curlIp: "http://worldtimeapi.org/api/ip",
-        ip:""
+        curlIp: "http://worldtimeapi.org/api/ip/",
+    },
+    geolocationDb:{
+      curl: "https://geolocation-db.com/json/ "
     },
     ipGeolocation:{
-        curl: "https://api.ipbase.com/v2/info?ip=1.1.1.1",
+        curl: "https://api.ipbase.com/v2/info?ip=",
         key: "5DW195dzlSfewvalyEXcVCmP5qbOcDUQnwX2p06V"
     },
     programmingCotes:{
