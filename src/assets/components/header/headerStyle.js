53534d8229d7a93edf40fc63048eec251069f505
Marco Franca <dev.marcotullio@gmail.com>
2022-10-19 02:20:58 -0300

    tablet responsiveness improvement [UPDATE]

--- src/assets/components/header/headerStyle.js
+++ src/assets/components/header/headerStyle.js
@@ -8,7 +8,7 @@ export const HeaderContainer = styled.header`
   padding: 32px 26px;
 
   .quotes {
-    max-width: 80%;
+    max-width: 85%;
     h1 {
       font-weight: 400;
       font-size: 12px;
@@ -28,7 +28,6 @@ export const HeaderContainer = styled.header`
     margin-left: 15px;
     margin-top: 5px;
     filter: drop-shadow(0px 2px 2px rgba(0, 0, 0, 1));
-
   }
 
   img {
@@ -39,49 +38,42 @@ export const HeaderContainer = styled.header`
 
   @media screen and (min-width: 700px) {
     padding: 80px 64px;
-
     .quotes {
-      
+      h1 {
+        margin: 0;
+        font-weight: 400;
+        font-size: 18px;
+        line-height: 28px;
+      }
+
+      p {
+        margin-top: 13px;
+        font-weight: 700;
+        font-size: 18px;
+        line-height: 28px;
+      }
     }
     .image {
       width: 17px;
       margin-left: 5px;
       margin-top: 5px;
-      filter: drop-shadow(0px 2px 2px rgba(0, 0, 0, 1));
-
     }
   }
   
   @media screen and (min-width: 1024px) {
     display: flex;
     position: relative;
-    padding-top: 66.67px;
+    padding-top: 67px;
 
   .quotes {
-    margin-left: 165px;
+    margin-left: 98px;
     max-width: 45%;
-
-    h1 {
-      margin: 0;
-      font-weight: 400;
-      font-size: 18px;
-      line-height: 28px;
-    }
-
-    p {
-      margin-top: 13px;
-      font-weight: 700;
-      font-size: 18px;
-      line-height: 28px;
-    }
   }
 
   .image {
     width: 20px;
     margin-left: 5px;
     margin-top: 5px;
-    filter: drop-shadow(0px 2px 2px rgba(0, 0, 0, 1));
-    
   }
   img {
     width: 100%;
@@ -92,7 +84,6 @@ export const HeaderContainer = styled.header`
       opacity: 1;
       color: white;
       mix-blend-mode: unset;
-      
     }
   }
 }
