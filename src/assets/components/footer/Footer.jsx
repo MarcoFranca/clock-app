c28fc847b4dd137b3658d59d8a6011341332fb41
Marco Franca <dev.marcotullio@gmail.com>
2022-10-19 02:19:44 -0300

    footer was changed from file[ADD]

---
+++ src/assets/components/footer/Footer.jsx
@@ -0,0 +1,21 @@
+import {useSelector} from "react-redux";
+import {FooterStiled, TimeInfo} from "./footerStyled";
+
+export function Footer (){
+    const timeStatus = useSelector((state)=> state.date.worldTime)
+    const backGround = useSelector((state)=> state.date.backgroundColor)
+    const textColor = useSelector((state)=> state.date.textColor)
+
+    return(
+        <FooterStiled background={backGround}>
+            <div>
+                <TimeInfo textColor={textColor}><h2>Current timezone</h2><p>{timeStatus.timezone}</p></TimeInfo>
+                <TimeInfo> <h2>Day of the year</h2><p>{timeStatus.day_of_year}</p></TimeInfo>
+            </div>
+            <div>
+                <TimeInfo><h2>Day of the week</h2><p>{timeStatus.day_of_week}</p></TimeInfo>
+                <TimeInfo><h2>Week number</h2><p>{timeStatus.week_number}</p></TimeInfo>
+            </div>
+        </FooterStiled>
+    )
+}
